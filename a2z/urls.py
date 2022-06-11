f18ac71697945c2197d5f0f4f5d64c762ba34e73
khirod-datascience <66660324+khirod-datascience@users.noreply.github.com>
2022-06-11 18:15:50 +0530

    Update urls.py

--- a2z/urls.py
+++ a2z/urls.py
@@ -17,15 +17,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.defaults import patterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('a2zapp.urls')),
 ]
-urlpatterns += patterns('',
-        (r'^static/(settings.STATIC_URL)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+if not settings.DEBUG:
+    urlpatterns += (r'^static/(settings.STATIC_URL)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
